1dcb6e48d69b6065e7f428864a11a9a84fe5471b
Hiroshi TAKEMOTO <take@pwv.co.jp>
2013-07-20 13:33:23 +0900

    change make files

--- LBED_MAIN/Debug/src/subdir.mk
+++ LBED_MAIN/Debug/src/subdir.mk
@@ -23,7 +23,7 @@ CPP_DEPS += \
 src/%.o: ../src/%.cpp
 	@echo 'Building file: $<'
 	@echo 'Invoking: MCU C++ Compiler'
-	arm-none-eabi-c++ -D__NEWLIB__ -D__USE_CMSIS=CMSISv2p00_LPC11xx -DDEBUG -I"/Users/take/proj/LBED/LBED_Proj/CMSISv2p00_LPC11xx/inc" -I"/Users/take/proj/LBED/LBED_Proj/LBED_LPC11xx/inc" -I"/Users/take/proj/LBED/LBED_Proj/LBED_LPC11xx/src/Common/inc" -I"/Users/take/proj/LBED/LBED_Proj/LBED_MAIN/inc" -I"/Users/take/proj/LBED/LBED_Proj/LBED_USERLIB/inc" -O0 -g -Wall -c -fmessage-length=0 -fno-builtin -ffunction-sections -fdata-sections -fno-rtti -fno-exceptions -mcpu=cortex-m0 -mthumb -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o"$@" "$<"
+	arm-none-eabi-c++ -D__NEWLIB__ -D__USE_CMSIS=CMSISv2p00_LPC11xx -DDEBUG -I"/Users/take/proj/LBED/lbed/CMSISv2p00_LPC11xx/inc" -I"/Users/take/proj/LBED/lbed/LBED_LPC11xx/inc" -I"/Users/take/proj/LBED/lbed/LBED_LPC11xx/src/Common/inc" -I"/Users/take/proj/LBED/lbed/LBED_MAIN/inc" -I"/Users/take/proj/LBED/lbed/LBED_USERLIB/inc" -O0 -g -Wall -c -fmessage-length=0 -fno-builtin -ffunction-sections -fdata-sections -fno-rtti -fno-exceptions -mcpu=cortex-m0 -mthumb -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o"$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
